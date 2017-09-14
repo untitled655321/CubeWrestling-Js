7b947c81e549661940a8474151b167a89c416056
Pawel <pawel.iwanczux@gmail.com>
2017-09-14 11:54:09 +0200

    dfaf

--- public/cube1.js
+++ public/cube1.js
@@ -191,7 +191,7 @@ function Make_player_tru(x,y,z,rot_x,rot_y,rot_z,rot_w)
 
 
 function init(){
-	var https = 'localhost:5000'; //https://cubewrestling.herokuapp.com/
+	var https = 'https://cubewrestling.herokuapp.com/'; //https://cubewrestling.herokuapp.com/
 	socket = io.connect(https);
 	//socket.on('cube', drawCube);
 	
